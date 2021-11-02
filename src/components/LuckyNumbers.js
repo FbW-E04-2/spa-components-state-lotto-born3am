3f60bee4b60a21b34d8d28efd56327aef667bf9e
born3am <psmaranhao@gmail.com>
2021-11-02 13:58:06 +0100

    react hook added

--- src/components/LuckyNumbers.js
+++ src/components/LuckyNumbers.js
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import Number from "./Number";
-export default class LuckyNumbers extends Component {
-    state = {
-        arrayNumber: [],
-        specialNumber: ""
-    };
-    generateLuckyNumber = () => {
+
+export default function LuckyNumbers() {
+
+    const [arrayNumber, setArrayNumber] = useState([])
+
+    const [specialNumber, setSpecialNumber] = useState("")
+
+
+    const generateLuckyNumber = () => {
         let luckyNumbers = [];
         for (let index = 0; index < 6; index++) {
             let randomNumber = Math.floor(Math.random() * 49 + 1);
@@ -14,42 +17,44 @@ export default class LuckyNumbers extends Component {
             }
             luckyNumbers.push(randomNumber);
         }
-        this.setState({
-            arrayNumber: luckyNumbers,
-            specialNumber: Math.floor(Math.random() * 10 + 1)
-        });
-    };
-    resetNumbers = () => {
-        this.setState({
-            arrayNumber: [],
-            specialNumber: ""
-        });
-    };
-    render() {
-        return (
-            <div>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-around",
-                        alignItems: "center",
+        setArrayNumber(
+            luckyNumbers,
+        );
 
-                        padding: "20px"
-                    }}
-                >
-                    {this.state.arrayNumber.map((currentValue) => {
-                        return <Number number={currentValue} />;
-                    })}
-                    {/*   {this.state.specialNumber && (
+        setSpecialNumber(
+            Math.floor(Math.random() * 10 + 1)
+        );
+    };
+
+    const resetNumbers = () => {
+        setArrayNumber([]);
+        setSpecialNumber("")
+
+    };
+
+    return (
+        <div>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center",
+
+                    padding: "20px"
+                }}
+            >
+                {arrayNumber.map((currentValue) => {
+                    return <Number number={currentValue} />;
+                })}
+                {/*   {this.state.specialNumber && (
             <Number number={this.state.specialNumber} />
           )} */}
-                    {this.state.specialNumber !== "" ? (
-                        <Number number={this.state.specialNumber} />
-                    ) : null}
-                </div>
-                <button onClick={this.resetNumbers}>Reset</button>
-                <button onClick={this.generateLuckyNumber}>Lucky Number</button>
+                {specialNumber !== "" ? (
+                    <Number number={specialNumber} />
+                ) : null}
             </div>
-        );
-    }
+            <button onClick={resetNumbers}>Reset</button>
+            <button onClick={generateLuckyNumber}>Lucky Number</button>
+        </div>
+    );
 }
